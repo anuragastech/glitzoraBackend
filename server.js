92ee48c8053be7243e7ab87c0342cb59c5e6ddec
Anurag <anuragraveendren98@gmail.com>
2026-05-05 22:24:52 +0530

    fix cors setup

--- server.js
+++ server.js
@@ -9,14 +9,22 @@ connectDB();
 const app = express();
 
 // ✅ CORS (ONLY ONCE)
-app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    "https://glitzora.vercel.app"
-  ],
-  methods: ["GET", "POST", "DELETE"],
-  allowedHeaders: ["Content-Type"]
-}));
+// app.use(cors({
+//   origin: [
+//     "http://localhost:5173",
+//     "https://glitzora.vercel.app"
+//   ],
+//   methods: ["GET", "POST", "DELETE"],
+//   allowedHeaders: ["Content-Type"]
+// }));
+
+// Middleware
+app.use(cors());
+// app.use(cors({
+//     // origin: 'http://localhost:3000', // React frontend URL
+//     methods: 'GET,POST,PUT,DELETE',
+//     allowedHeaders: 'Content-Type,Authorization'
+//   }));
 
 app.use(express.json());
 
